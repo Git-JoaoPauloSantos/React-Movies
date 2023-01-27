a15465649bb334d90e3078243734341dd6dd3e69
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-01-27 00:37:14 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import './Navbar.css'
+import { Link, useNavigate } from "react-router-dom"
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { useState } from 'react'
+
+const Navbar = () => {
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        if(!search) return
+
+        navigate(`/search?q=${search}`)
+        setSearch("")
+    }
+
+    return (
+        <nav id='navbar'>
+            <h2> <Link to={"/"}> <BiCameraMovie /> ReactMovies</Link> </h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Pesquisar..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button type="submit"> <BiSearchAlt2 /> </button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar
