ef2be0544c638ea0f64752f2881c14ef92487913
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-01-29 03:27:13 -0300

    Add função carrossel nas listas de filmes

--- src/components/ListMovies.jsx
+++ src/components/ListMovies.jsx
@@ -1,24 +1,36 @@
 import './ListMovies.css'
 import { MdNavigateBefore } from 'react-icons/md';
 import { MdNavigateNext } from 'react-icons/md';
+import { useRef } from 'react';
 
 const imageUrl = import.meta.env.VITE_IMG
 
 const ListMovies = ({ title, body }) => {
+    const listMovies = useRef(null)
+
+    const handleLeftClick = () => {
+
+        listMovies.current.scrollLeft -= listMovies.current.offsetWidth
+    }
+
+    const handleRightClick = () => {
+
+        listMovies.current.scrollLeft += listMovies.current.offsetWidth
+    }
+
     return (
         <div className='list-movies-container'>
             <h2>{title}</h2>
-            <div className="btn-left"><MdNavigateBefore /></div>
-            <div className="btn-right"><MdNavigateNext /></div>
-            <div className="list-movies-listarea">
-                <div className="list-movies">
-                    {body.results.length > 0 && body.results.map((movie) => (
-                        <div key={movie.id} className="movie">
-                            <img src={imageUrl + movie.backdrop_path} alt={movie.name || movie.title} />
-                        </div>
-                    ))}
-                </div>
+            <button className="btn-left" onClick={handleLeftClick}><MdNavigateBefore /></button>
+            <button className="btn-right" onClick={handleRightClick}><MdNavigateNext /></button>
+            <div className="list-movies" ref={listMovies}>
+                {body.results.length > 0 && body.results.map((movie) => (
+                    <div key={movie.id} className="movie">
+                        <img src={imageUrl + movie.backdrop_path} alt={movie.name || movie.title} />
+                    </div>
+                ))}
             </div>
+
         </div>
     )
 }
